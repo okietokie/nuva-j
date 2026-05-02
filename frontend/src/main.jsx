456c6e9b03b4e1ac03a42a9cc23208e7eb0bc683
okietokie <kalyanisanthoshkumar@gmail.com>
2026-05-02 22:53:56 +0400

    test push

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ConfigProvider theme={theme}>
       <AuthProvider>
         <CartProvider>
+          
           <RouterProvider router={router} />
         </CartProvider>
       </AuthProvider>
